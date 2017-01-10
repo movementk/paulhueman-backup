900654710822cb1c31f101effee09c8a64709bba
kyh5392 <kyh90495392@gmail.com>
2017-01-10 10:45:47 +0900

    서브가이드 작성 시작

---
+++ sub.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>폴 휴먼</title>
+    <link href="/assets/fontello/css/fontello.css" rel="stylesheet">
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/default.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <header id="header">
+        <div class="container">
+            Header
+        </div>
+    </header>
+    <main id="content">
+        <div class="container">
+            Main Content
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            Footer
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
