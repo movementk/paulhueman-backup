db9c56f72f6d4d8b750580a3399f8b199b55c772
mk-kimjh <mk.kimjh@gmail.com>
2017-01-10 10:11:21 +0900

    초기화

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>폴 휴먼</title>
+    <link href="/assets/fontello/css/fontello.css" rel="stylesheet">
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/default.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <header id="header">
+        <div class="top-nav visible-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-6">
+                        GLOBAL SHIPPING
+                    </div>
+                    <div class="col-xs-6">
+                        마이페이지
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="logo-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <button type="buton" class="btn btn-open-aside hidden-lg"><span class="sr-only">네비버튼</span></button>
+                    </div>
+                    <div class="col-xs-4">
+                        <h1><a href="#"><img src="/assets/images/logo.gif" alt="Paul Hueman PARIS"></a></h1>
+                    </div>
+                    <div class="col-xs-4">
+                        <a href="#" class="btn btn-cart"><span class="sr-only">장바구니</span></a>
+                        <button type="buton" class="btn btn-search"><span class="sr-only">검색</span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav id="gnb">
+            <div class="container">
+                <ul>
+                    <li>
+                        <a href="#">STORY</a>
+                        <ul class="visible-lg">
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                        <ul class="visible-lg">
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">CAMPAIGN</a>
+                        <ul class="visible-lg">
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">STOCKISTS</a>
+                        <ul class="visible-lg">
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                            <li><a href="#">sub-1</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <aside id="aside" class="hidden-lg">
+    </aside>
+    <main id="content">
+        <div class="container">
+            #Aaaaaaaaaaa
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
