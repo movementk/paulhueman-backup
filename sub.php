c2e92d0f139829278a5ed6636473edb6829665e8
kyh5392 <kyh90495392@gmail.com>
2017-01-11 18:38:54 +0900

    서브 가이드 퍼블리싱 작업중...

--- sub.php
+++ sub.php
@@ -1,105 +1,13 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>폴 휴먼</title>
-    <link href="/assets/fontello/css/fontello.css" rel="stylesheet">
-    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/css/default.css" rel="stylesheet">
-    <link href="/assets/css/sub.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
 </head>
 <body class="sub">
-    <!-- 서브가이드 페이지 완료시 include 하기 -->
-    <header id="header">
-        <div class="top-nav visible-lg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-6">
-                        <div class="global">
-                            <a href="#">GLOBAL SHIPPING</a>
-                        </div>
-                    </div>
-                    <div class="col-xs-6">
-                        <div class="">
-                            마이페이지
-                        </div>
-                        <a href=""></a>
-                        <div class="search">
-                            <form>
-                                <button class="btn btn-search" type="submit"><span class="sr-only">검색</span></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="logo-nav">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-4 hidden-lg">
-                        <button type="button" class="btn btn-open-aside">
-                            <span class="sr-only">네비버튼</span>
-                        </button>
-                    </div>
-                    <div class="col-xs-4 col-lg-4 col-lg-offset-4">
-                        <h1><a href="/"><img src="/assets/images/logo.gif" alt="Paul Hueman PARIS"></a></h1>
-                    </div>
-                    <div class="col-xs-4 hidden-lg">
-                        <a href="#" class="btn btn-cart"><span class="sr-only">장바구니</span></a>
-                        <button type="button" class="btn btn-search"><span class="sr-only">검색</span></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <nav id="gnb">
-            <div class="container">
-                <ul class="row">
-                    <li class="col-sm-3 col-lg-2 col-lg-offset-2">
-                        <a href="#">STORY</a>
-                        <ul class="visible-lg">
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                        </ul>
-                    </li>
-                    <li class="col-sm-3 col-lg-2">
-                        <a href="#">SHOP</a>
-                        <ul class="visible-lg">
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                        </ul>
-                    </li>
-                    <li class="col-sm-3 col-lg-2">
-                        <a href="#">CAMPAIGN</a>
-                        <ul class="visible-lg">
-                            <li><a href="#">DIARY</a></li>
-                            <li><a href="#">VIDEO</a></li>
-                            <li><a href="#">CELEB</a></li>
-                            <li><a href="#">LOOKBOOK</a></li>
-                        </ul>
-                    </li>
-                    <li class="col-sm-3 col-lg-2">
-                        <a href="#">STOCKISTS</a>
-                        <ul class="visible-lg">
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                            <li><a href="#">sub-1</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header><!-- //header -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    
+    
+    <aside id="aside" class="hidden-lg"></aside>
+    
+    
     
     <main id="content">
         <div class="container">
@@ -234,33 +142,7 @@
             
         </div><!--// Container -->
     </main><!--// Main Content -->
-    
-    <footer id="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-9">
-                    <div class="company">
-                        <p>COPYRIGHT © PAULHUEMAN</p>
-                        <ul>
-                            <li>BUSINESS RESISTRATION No.504 18 32028</li>
-                            <li>E-COMMERCE PERMIT : 1234-56-78901</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="policy">
-                        <ul>
-                            <li><a href="#">CONTACT</a></li>
-                            <li><a href="#">TERMS</a></li>
-                            <li><a href="#">CUSTOMER</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/assets/js/common.js"></script>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
 </body>
 </html>
